4c4d1325d1ccad3648bddefd74f64e813487cc92
Anderson <andersonfonseca1999@hotmail.com>
2021-11-13 18:28:03 -0300

    Add middleware to validate id

---
+++ api/src/middlewares/TodoMiddleware.js
@@ -0,0 +1,23 @@
+const { validate: isUuid } = require('uuid');
+const Todo = require('../models/Todo');
+
+module.exports = {
+  async validadeId(request, response, next) {
+    const { id } = request.params;
+
+    if (!isUuid(id)) return response.status(400).json({ error: 'Invalid ID.' });
+
+    try {
+      const todo = await Todo.findById(id);
+      response.todo = todo;
+
+      if (!todo) {
+        return response.status(400).json({ error: 'Todo not found.' });
+      }
+    } catch (error) {
+      return response.status(500).json({ error: error.message });
+    }
+
+    next();
+  },
+};
